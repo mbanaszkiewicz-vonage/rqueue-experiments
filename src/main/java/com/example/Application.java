8f286cd53e2f51b4dee8aa53d0a2d9b89e382137
mbanaszkiewicz-vonage <mateusz.banaszkiewicz@vonage.com>
2024-10-23 10:16:53 +0200

    try 2 options

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,11 +1,24 @@
 package com.example;
 
+import com.github.sonus21.rqueue.config.SimpleRqueueListenerContainerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
+
+  @Bean
+  public SimpleRqueueListenerContainerFactory simpleRqueueListenerContainerFactory() {
+    SimpleRqueueListenerContainerFactory simpleRqueueListenerContainerFactory =
+        new SimpleRqueueListenerContainerFactory();
+    simpleRqueueListenerContainerFactory.setPollingInterval(1000);
+    return simpleRqueueListenerContainerFactory;
+  }
+
   public static void main(String[] args) {
     SpringApplication.run(Application.class, args);
   }
+
+
 }
--- src/main/java/com/example/JobConsumer.java
+++ src/main/java/com/example/JobConsumer.java
@@ -9,7 +9,7 @@ import java.time.Instant;
 
 @Service
 public class JobConsumer {
-  @RqueueListener(value = "my-delay-queue")
+  @RqueueListener(value = "my-delay-queue", visibilityTimeout = "10000")
   public void handleMessage(String msg, @Header(RqueueMessageHeaders.ID) String messageId) {
     System.out.println("%s Handling message (id: %s, body: %s)".formatted(Instant.now(), messageId, msg));
   }
